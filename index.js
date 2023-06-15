f35e3f190fa9cda3006565ca16718e72e5cd8286
rater7tara <nowshinakteremu005@gmail.com>
2023-06-15 23:30:59 +0600

    file change

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function run() {
 
       app.post('/jwt', (req, res) => {
         const user = req.body;
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
   
         res.send({ token })
       })
